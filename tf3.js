4f7c1c53068b0c27c3f472ebea9976495eda92f4
Shamibei <shamibei2003@gmmail.com>
2023-01-27 15:44:43 +0300

    edits

--- tf3.js
+++ tf3.js
@@ -22,7 +22,6 @@ app.get('/generate-jwt', async (req, res) => {
   }
 });
 
-// Generate JWT token every hour
 setInterval(() => {
   app.get('/generate-jwt');
 }, 60 * 60 * 1000);
@@ -31,20 +30,5 @@ app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
 
-
-/*
-Install the required modules by running npm install request-promise data-store
-Import the modules in your code using const request = require('request-promise'); const DataStore = require('data-store');
-Create a new instance of the DataStore module to store the JWT token: const store = new DataStore({ path: 'path/to/store/file.json' });
-Use the request module to make a POST request to the Metabase API endpoint for generating JWT tokens, 
-passing in the necessary authentication credentials. 
-This endpoint is typically http://<your-metabase-server>/api/session
-In the response, the JWT token will be returned in the id field of the JSON object. 
-Store this token in the DataStore instance using store.set('jwt', response.id);
-To retrieve the JWT token from the DataStore, you can use const jwt = store.get('jwt');
-To generate the JWT token on an hourly basis, you can use the built-in setInterval function in Node.js to call the code for generating and storing the token every hour.
-
-Here is an example of the complete code for generating and storing a JWT token using Node.js, express.js, and the request and data-store modules:
- */
 
 
